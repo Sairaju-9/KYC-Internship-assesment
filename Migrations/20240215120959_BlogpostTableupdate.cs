956d611f91af52b57e0e2c5d28f0a732b8978bcb
k.jyothi sai raju <113966385+Sairaju-9@users.noreply.github.com>
2024-02-29 15:48:32 +0530

    Add files via upload

---
+++ Migrations/20240215120959_BlogpostTableupdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Code_plus.Migrations
+{
+    /// <inheritdoc />
+    public partial class BlogpostTableupdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "content",
+                table: "BlogPosts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "content",
+                table: "BlogPosts");
+        }
+    }
+}
